61d7baac5466b973b8b486d2a656252439d4d6ae
Minura <mnurasha2000@gmail.com>
2024-10-17 23:28:47 +0530

    out the button

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,9 +4,13 @@ function Searchbar() {
   return (
     <div>
       <form action="/search" method="GET">
-        <input type="text" name="query" placeholder="Search here..." required>
-          <button type="submit">Search</button>
-        </input>
+        <input
+          type="text"
+          name="query"
+          placeholder="Search here..."
+          required
+        ></input>
+        <button type="submit">Search</button>
       </form>
     </div>
   );
