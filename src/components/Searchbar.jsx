7ac36952242ec328e9f66bb0fe4c3c47c6340564
Minura <mnurasha2000@gmail.com>
2024-10-17 23:53:49 +0530

    new

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function Searchbar() {
   return (
     <div>
-        <div>
-            <div>
-                <input type="text" placeholder="Search here..." required/>
-                <button type="submit">🔍</button>
-            </div>
+      <div>
+        <div class="w-full max-w-md">
+          <form action="" class="relative flex items-center">
+            <input type="text" placeholder="Search here..."  class="w-full px-4 py-2 pr-12 rounded-full border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none" required />
+            <button type="submit"class="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white rounded-full p-2 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 focus:outline-none">🔍</button>
+          </form>
         </div>
+      </div>
     </div>
   );
 }
