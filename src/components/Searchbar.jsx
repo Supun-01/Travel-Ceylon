ee2810297eee39ff5b380c0b7827a9e3592cefae
Minura <mnurasha2000@gmail.com>
2024-10-17 23:22:35 +0530

    changed

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 
-
 function Searchbar() {
   return (
     <div>
       <form action="/search" method="GET">
-        <input
-          type="text"
-          name="query"
-          placeholder="Search here..."
-          required
-        >
-        <button type="submit">Search</button>
+        <input type="text" name="query" placeholder="Search here..." required>
+          <button type="submit">Search</button>
         </input>
-        
       </form>
     </div>
   );
