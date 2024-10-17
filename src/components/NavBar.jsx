98ea2f4ef6ded7cd88cb82ab8f6a30a77ae0476b
Supun <Supuntharakaxz@gamil.com>
2024-10-17 10:50:08 +0530

    NavBar v01

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,28 @@
 function NavBar() {
     return (
-        0
+        <div className="border border-black px-8 py-5 lg:px-20 lg:py-10 bg-white">
+            <navbar className="flex flex-wrap justify-between">
+                <div className=" md:text-3xl lg:text-4xl text-pink-600">
+                    TravelCeylon
+                </div>
+
+                <div className="flex gap-4 md:text-2xl lg:text-3xl text-pink-800">
+                    <div>
+                        Home
+                    </div>
+                    <div>
+                        Booking
+                    </div>
+                    <div>
+                        About Us
+                    </div>
+                    <div>
+                        Contact
+                    </div>
+                </div>
+
+            </navbar>
+        </div>
     )
 }
 
