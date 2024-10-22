e37c3e7e67656e7cb551ac72097bf51cc8ad0ce6
Minura <mnurasha2000@gmail.com>
2024-10-22 12:17:33 +0530

    hero two adding

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar"
 import CardContainer from "./components/CardComponents/CardContainer";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
+import Herosectwo from "./components/Herosectwo";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <NavBar />
       <div className="mx-8 lg:mx-32">
         <HeroSection />
+        <Herosectwo/>
         <CardContainer />
       </div>
       <Footer />
--- src/components/Herosectwo.jsx
+++ src/components/Herosectwo.jsx
@@ -3,6 +3,18 @@ import React from 'react'
 function Herosectwo() {
   return (
     <div>
+        <div className='flex flex-col'>
+            {/* tittle bar */}
+            <div>why you want to select us</div>
+
+            <div>
+                <div>item 1</div>
+                <div>item 2</div>
+                <div>item 3</div>
+            </div>
+
+
+        </div>
       
     </div>
   )
